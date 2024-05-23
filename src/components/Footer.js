3b1d0e099dc27e9814b00692c23a11721184606a
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-23 10:26:06 +0100

    Fixing homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>
-      <div className="container">
-        <div className="ptb-2">
-          Developed with Love by{" "}
-          <Link to="https://nikitamallya.info" target="_blank">
-            Nikita
-          </Link>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="ptb-2">
+            Developed with Love by{" "}
+            <Link to="https://nikitamallya.info" target="_blank">
+              Nikita
+            </Link>
+          </div>
         </div>
       </div>
     </div>
