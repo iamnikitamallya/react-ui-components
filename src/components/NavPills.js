3a496ec1b0b82ab5afcaf9d7ed75487679401e00
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-24 15:08:40 +0100

    Completed Nav Pills Component

--- src/components/NavPills.js
+++ src/components/NavPills.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function NavPills() {
+  const tabs = [
+    {
+      id: "tab1",
+      title: "Tab 1",
+      content: "You will always regret the round of JÃ¤germeister.",
+    },
+    {
+      id: "tab2",
+      title: "Tab 2",
+      content: "If you don't ask, you don't get.",
+    },
+    {
+      id: "tab3",
+      title: "Tab 3",
+      content: "When faced with a choice, do both.",
+    },
+    ];
+    
+    const [activeTab, setActiveTab] = useState('tab1')
+    const handleClick = (id) => {
+        setActiveTab(id)
+    }
+  
   return (
-    <div>
-      <button>Nav 1</button>
-      <button>Nav 2</button>
-      <button>Nav 3</button>
-    </div>
+    <>
+      <div className="tab-header">
+        {tabs.map((item) => (
+          <button
+            key={item.id}
+            onClick={() => handleClick(item.id)}
+            className={activeTab === item.id ? "active" : ""}
+          >
+            {item.title}
+          </button>
+        ))}
+      </div>
+      <div className="card tab-content p-2">
+        {tabs.map((item) => activeTab === item.id ? (
+            <div key={item.id}>{ item.content}</div>
+        ) : null)}
+      </div>
+    </>
   );
 }
