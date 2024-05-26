caaa22cab9eab8a8eee1fb8b46d2f85ac0baf6df
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-26 22:04:45 +0100

    Fixing components

--- src/pages/Components.js
+++ src/pages/Components.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavPills from "../components/NavPills";
 import Accordion from "../components/Accordion";
 import AccordionV2 from "../components/AccordionV2";
+import CardV1 from "../components/CardV1";
 
 export default function Components() {
   const UIComponents = [
@@ -16,13 +17,17 @@ export default function Components() {
     {
       title: "Accordion v.2",
       components: <AccordionV2 />,
-    }
+    },
+    {
+      title: "Card v.1",
+      components: <CardV1 />,
+    },
   ];
   return (
     <div className="container pt-4 pb-4 components">
-      <div className="item">
+      <div className="row">
         {UIComponents.map((item) => (
-          <div className="items">
+          <div className="col-md-4 pb-4">
             <div className="card card-primary p-3">
               <h3>{item.title}</h3>
               {item.components}
