34fa295bf30aa2046bd1ead2df30d8f95277bc72
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-26 21:55:38 +0100

    Adding cards component

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Cards() {
+  return (
+      <>
+          <div className="cards">
+              <h3>Title</h3>
+              <p>Content Goes Here</p>
+      </div>
+      </>
+  )
+}
