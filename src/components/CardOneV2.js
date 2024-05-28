060a0bc201b1504aafabc87269a73535f152120e
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-28 20:56:55 +0100

    Adding button component to cards

--- src/components/CardOneV2.js
+++ src/components/CardOneV2.js
@@ -6,7 +6,7 @@ export default function CardOneV2() {
     <>
       <div className="card p-2 text-center">
         <h3>Lorem Ipsum</h3>
-        <p>
+        <p className="pb-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
