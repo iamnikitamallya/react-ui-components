163d2c38e78925e9ef6366d21cf5d7e035b0f0ba
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-28 21:03:34 +0100

    Adding btn to card 4

--- src/components/CardThreeV1.js
+++ src/components/CardThreeV1.js
@@ -1,5 +1,6 @@
 import React from "react";
 import card from "../assets/img/card.jpg";
+import Button from "./Button";
 
 export default function CardThreeV1() {
   return (
@@ -9,10 +10,11 @@ export default function CardThreeV1() {
           <div className="col-md-12">
             <img src={card} alt="" className="w-50 mb-3" />
             <h3>Lorem Ipsum</h3>
-            <p>
+            <p className="pb-2">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
+            <Button />
           </div>
         </div>
       </div>
