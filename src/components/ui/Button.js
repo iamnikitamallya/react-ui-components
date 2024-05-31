b91d45167d1b2cbfe03b401214b65857ee4ced6f
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-31 22:23:20 +0100

    Fixing card code

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -13,7 +13,7 @@ export default function Button({variant, size, onClick, disabled, title, url, ic
 }
 
 Button.propTypes = {
-  variant: PropTypes.oneOf(['primary', 'secondary', 'light', 'dark', 'success', 'danger', 'warning']),
+  variant: PropTypes.oneOf(['primary', 'secondary', 'light', 'dark', 'success', 'danger', 'warning', 'info']),
   size: PropTypes.oneOf(['sm', 'lg']),
   disabled: PropTypes.bool,
   onClick: PropTypes.func
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,5 +1,4 @@
-import React, { Children } from "react";
-import Button from "./Button";
+import React from "react";
 import PropTypes from "prop-types";
 
 export default function Card({
