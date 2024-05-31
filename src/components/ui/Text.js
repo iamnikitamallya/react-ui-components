e591b6c89097cb49e65715e0e01ef479696f5255
iamnikitamallya <mallyanikita165@gmail.com>
2024-05-31 23:22:03 +0100

    Adding multiple features to TEXT component

--- src/components/ui/Text.js
+++ src/components/ui/Text.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
 export default function Text({
   title,
@@ -7,23 +8,63 @@ export default function Text({
   weight,
   decoration,
   style,
-  lineHeight,
   letterSpacing,
+  align,
+  transform
 }) {
   return (
     <>
       <div
-        className={`text-${color} ${variant}`}
+        className={`text-${color} ${variant} text-${align} text-${transform} fst-${style} fw-${weight} text-decoration-${decoration}`}
         style={{
-          fontWeight: `${weight}`,
-          textDecoration: `${decoration}`,
-          fontStyle: `${style}`,
-          lineHeight: `${lineHeight}`,
-          letterSpacing: `${letterSpacing}`,
+          letterSpacing: `${letterSpacing}px`,
         }}
       >
         {title}
       </div>
     </>
   );
+}
+
+Text.propTypes = {
+  color: PropTypes.oneOf([
+    "primary",
+    "secondary",
+    "light",
+    "dark",
+    "white",
+    "warning",
+    "success",
+    "danger",
+    "info",
+    "body",
+    "muted",
+    "black-50",
+    "white-50",
+  ]),
+  variant: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6", "para"]),
+  align: PropTypes.oneOf([
+    "start",
+    "center",
+    "end",
+    "left",
+    "right",
+    "justify",
+  ]),
+  transform: PropTypes.oneOf(["lowercase", "uppercase", "capitalize", "none"]),
+  style: PropTypes.oneOf(["normal", "italic"]),
+  weight: PropTypes.oneOf(["bold", "bolder", "normal", "light", "lighter"]),
+  decoration: PropTypes.oneOf(["underline", 'line-through', 'none']),
+  letterSpacing: PropTypes.oneOf(['0', '1', '2', '3', '4', '5'])
+};
+
+Text.defaultProps = {
+  color: 'body',
+  variant: 'para',
+  align: 'start',
+  transform: 'none',
+  style: 'normal',
+  weight: 'normal',
+  decoration: 'none',
+  letterSpacing: '0'
 }
